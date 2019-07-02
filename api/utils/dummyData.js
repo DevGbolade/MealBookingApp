741dfcfc8c2fd1bc22775259276af625bdb4d47f
rozay10 <raadeniyi3@gmail.com>
2019-07-02 12:05:55 +0100

    create meal services

---
+++ api/utils/dummyData.js
@@ -0,0 +1,28 @@
+export default {
+    meals: [
+        {
+            id: 1,
+            name:'Fried rice',
+            size:'medium',
+            price: 500
+        },
+        {
+            id: 2,
+            name:'jollof rice',
+            size:'large',
+            price: 800
+        },
+        {
+            id: 3,
+            name:'beans',
+            size:'small',
+            price: 150
+        },
+        {
+            id:4,
+            name:'Eba and soup',
+            size:'medium',
+            price: 500
+        }
+    ]
+}
