8822bc1bf7a0b4f847b172f57634e2fac9d84e9e
rozay10 <raadeniyi3@gmail.com>
2019-07-02 19:06:36 +0100

    create meal route

---
+++ api/controllers/meal.controller.js
@@ -0,0 +1,45 @@
+import mealService from '../services/meal.services';
+
+const mealController = {
+    fetchAllMeals(req, res){
+        const allMeals = mealService.fetchAllMeals();
+        return res.json({
+            status: "success",
+            data:  allMeals
+
+        }).status(200);
+    },
+
+     /**
+      * @addMealController
+         *Expect json of this format
+         * {
+         *  name:  "some food"
+         *  size:  "large"
+         * "price": "500"
+         * }
+         * 
+    */
+
+    addMeal(req, res){
+        const newMeal = req.body;
+        const createdMeal = mealService.addMeal(newMeal);
+        return res.json({
+            status: "success",
+            data: createdMeal
+        }).status(201);
+    },
+
+    getSingleMeal(req, res){
+        const id = req.params.id;
+        const foundMeal = mealService.getAMeal(id);
+        console.log(foundMeal);
+        
+        return res.json({
+            status: "success",
+            data: foundMeal
+        }).status(200);
+    }
+
+}
+export default mealController;
--- api/index.js
+++ api/index.js
@@ -1,11 +1,16 @@
 import express from 'express';
-var app = express();
+import bodyParser from 'body-parser';
+import mealRoutes from './routes/meal.route'
+const app = express();
 
+app.use(bodyParser.json());
 const PORT = 1111;
 
 app.get('/', (req, res) => {
-res.send('Api is bootsraped and its ready to roll')
+res.send('Api is bootstraped and its ready to roll')
 })
+
+app.use('/api/v1/meals', mealRoutes);
 
 app.listen(PORT, () => {
     console.log('Server is running on port ' + PORT);
